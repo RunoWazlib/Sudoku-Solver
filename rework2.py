b24b31a8b8dcb1ba947dc764fd9e9a2e606bb17e
RunoWazlib <ethanrog01@gmail.com>
2023-12-27 23:33:06 -0600

    Fixed grids

--- rework2.py
+++ rework2.py
@@ -1,3 +1,4 @@
+from math import *
 class Puzzle():
     def __init__(self, input_puzzle):
         """
@@ -16,20 +17,24 @@ class Puzzle():
         self.columns = {}
         input_puzzle = input_puzzle.lower()
         
-        #get grids
-        for i in range(9):
-            if i < 1:
-                self.grids[i+1] = [input_puzzle[:9]]
-            else:
-                self.grids[i+1] = [input_puzzle[(i*9):(i*9+9)]]
-
         #get rows
         for i in range(9):
-            self.rows[i+1] = [
-                input_puzzle[(3*i):((3*i)+3)]
-                +input_puzzle[((3*i)+9):((3*i)+12)]
-                +input_puzzle[((3*i)+18):((3*i)+21)]
-                ]
+            if i < 1:
+                self.rows[i+1] = [input_puzzle[:9]]
+            else:
+                self.rows[i+1] = [input_puzzle[(i*9):(i*9+9)]]
+
+        #get grids
+        grid_assembly = []
+        i = 0
+        while i < len(input_puzzle):
+            grid_assembly.append([input_puzzle[i:i+3]])
+            i = i+3
+        
+        for grid_row in range(3):
+            for grid_column in range(3):
+                grid_assembly2 = grid_assembly[grid_column+(9*grid_row)]+grid_assembly[grid_column+3+(9*grid_row)]+grid_assembly[grid_column+6+(9*grid_row)]
+                self.grids[len(self.grids)+1] = grid_assembly2
 
         #get columns
         for column in range(9):
@@ -41,19 +46,34 @@ class Puzzle():
 
         #get unknown values + get indexes
         self.number_unknown_values = input_puzzle.count('n')
-        try:
-            self.unknown_locations = input_puzzle.index('n')
-        except ValueError:
-            print("[-] No unknown values")
     
     def Guesser(self):
+        guesses = {}
         possible_values = [1,2,3,4,5,6,7,8,9]
-        while self.unknown_values > 0:
-            self.unknown_locations[0]
-            
+        while self.number_unknown_values > 0:
+            for i in range(1,9):
+                if self.grids[i][0].find('n') != -1:
+                    position = (self.grids[i][0].find('n'))+i*9
+                    guess = []
+                    # column_number = Position % 81
+
+                    guesses[position] = guess
+            break
+                    
+                
+
+
+#test            
 test = "123456789"*6+"nnnnnnnnn"*3
+#grid test
+test = "111222333111222333111222333444555666444555666444555666777888999777888999777888999"
 test = Puzzle(test)
+print("[*] Grids:")
 print(test.grids)
+print("[*] Rows:")
 print(test.rows)
+print("[*] Columns:")
 print(test.columns)
+print("[*] Number of 'n's:")
 print(test.number_unknown_values)
+test.Guesser()
